0739686ed70b29fc553c7c7270596a71451421f7
Stephen Warren <swarren@nvidia.com>
2016-02-12 14:35:08 -0700

    Enable use of SD card on EIMT Dalmore and p2371-0000
    SD cards have now been installed. Update the test config files to make
    use of them.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- py/tamien/u_boot_boardenv_dalmore_na.py
+++ py/tamien/u_boot_boardenv_dalmore_na.py
@@ -33,72 +33,69 @@ env__usb_dev_ports = (
 )
 
 env__block_devs = (
-#    # SD card; present since I plugged one in
-#    {
-#        "fixture_id": "sd",
-#        "type": "mmc",
-#        "id": "1",
-#        "writable_fs_partition": 1,
-#        "writable_fs_subdir": "tmp/",
-#    },
+    # SD card; present since I plugged one in
+    {
+        "fixture_id": "sd",
+        "type": "mmc",
+        "id": "1",
+        "writable_fs_partition": 1,
+        "writable_fs_subdir": "tmp/",
+    },
     # eMMC; always present
     {
         "fixture_id": "emmc",
         "type": "mmc",
         "id": "0",
-        # FIXME: Move to SD card ASAP
-        "writable_fs_partition": 1,
-        "writable_fs_subdir": "tmp/",
     },
 )
 
 env__dfu_configs = (
-#    # SD card, partition 1, ext4 filesystem
-#    {
-#        "fixture_id": "sd_fs",
-#        "alt_info": "/dfu_test.bin ext4 1 1;/dfu_dummy.bin ext4 1 1",
-#        "cmd_params": "mmc 1",
-#        "test_sizes": (
-#            64 - 1,
-#            64,
-#            64 + 1,
-#            4096 - 1,
-#        ),
-#    },
-#    # SD card, partition 3, partition
-#    {
-#        "fixture_id": "sd_part",
-#        "alt_info": "/dfu_test.bin part 1 3;/dfu_dummy.bin ext4 1 1",
-#        "cmd_params": "mmc 1",
-#        "test_sizes": (
-#            128 - 1,
-#            128,
-#            128 + 1,
-#            4096,
-#        ),
-#    },
-#    # SD card, partition 3, raw device (location overlays partition 3)
-#    {
-#        "fixture_id": "sd_raw",
-#        "alt_info": "/dfu_test.bin raw 4196352 18432;/dfu_dummy.bin ext4 1 1",
-#        "cmd_params": "mmc 1",
-#        "test_sizes": (
-#            960 - 1,
-#            960,
-#            960 + 1,
-#            4096 + 1,
-#        ),
-#    },
+    # SD card, partition 1, ext4 filesystem
+    {
+        "fixture_id": "sd_fs",
+        "alt_info": "/dfu_test.bin ext4 1 1;/dfu_dummy.bin ext4 1 1",
+        "cmd_params": "mmc 1",
+        "test_sizes": (
+            64 - 1,
+            64,
+            64 + 1,
+            4096 - 1,
+        ),
+    },
+    # SD card, partition 3, partition
+    {
+        "fixture_id": "sd_part",
+        "alt_info": "/dfu_test.bin part 1 3;/dfu_dummy.bin ext4 1 1",
+        "cmd_params": "mmc 1",
+        "test_sizes": (
+            128 - 1,
+            128,
+            128 + 1,
+            4096,
+        ),
+    },
+    # SD card, partition 3, raw device (location overlays partition 3)
+    {
+        "fixture_id": "sd_raw",
+        "alt_info": "/dfu_test.bin raw 4196352 18432;/dfu_dummy.bin ext4 1 1",
+        "cmd_params": "mmc 1",
+        "test_sizes": (
+            960 - 1,
+            960,
+            960 + 1,
+            4096 + 1,
+        ),
+    },
     # RAM
     {
         "fixture_id": "ram",
         "alt_info": "alt0 ram 80000000 01000000;alt1 ram 81000000 01000000",
         "cmd_params": "ram na",
-#        "test_sizes": (
-#            1024 * 1024 - 1,
-#            1024 * 1024,
-#            8 * 1024 * 1024,
-#        ),
+        "test_sizes": (
+            1024 * 1024 - 1,
+            1024 * 1024,
+            8 * 1024 * 1024,
+        ),
     },
 )
 
