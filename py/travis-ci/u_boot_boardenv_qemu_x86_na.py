2c2bcb42c2cd0f8e354e96315423c465e2f95187
Alexander Graf <agraf@suse.de>
2016-11-16 11:28:22 +0100

    Add efi_loader helloworld link to travis env
    We have a new test framework for efi_loader in test/py now. That framework
    can fetch a hello world application via tftp, but it needs to have it
    referenced in the environment.
    
    Fortunately U-Boot builds said hello world efi binary as part of its build
    process, so let's expose that to the test case.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- py/travis-ci/u_boot_boardenv_qemu_x86_na.py
+++ py/travis-ci/u_boot_boardenv_qemu_x86_na.py
@@ -4,3 +4,4 @@ env__net_uses_pci = True
 env__net_dhcp_server = True
 
 env__net_tftp_readable_file = travis_tftp.file2env('u-boot')
+env__efi_loader_helloworld_file = travis_tftp.file2env('lib/efi_loader/helloworld.efi')
