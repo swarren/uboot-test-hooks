68b9817137fc2118634f07c907f5e7bd69b7663d
Alexander Graf <agraf@suse.de>
2016-11-18 10:13:06 +0100

    Add travis tftp helper
    The following patches will allow us to share files between the
    build host and the qemu instance running our test case.
    
    To reduce code duplication, expose a simple helper wrapper that
    creates an env dict including checksum from a file name.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

---
+++ py/travis-ci/travis_tftp.py
@@ -0,0 +1,11 @@
+import os
+import binascii
+
+def file2env(file_name):
+    file_full = os.environ['UBOOT_TRAVIS_BUILD_DIR'] + "/" + file_name
+
+    return {
+        "fn": file_name,
+        "size": os.path.getsize(file_full),
+        "crc32": hex(binascii.crc32(open(file_full, 'rb').read()) & 0xffffffff)[2:],
+    }
