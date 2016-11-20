eab26e01583ed0d5a3d9ba90d289482541e38567
Alexander Graf <agraf@suse.de>
2016-11-20 09:48:23 +0100

    travis: Skip files that don't exist
    The travis tftp helper exposes files from the host into the guest environment.
    However, depending on the travis configuration and patch status, files may or
    may not exist yet.
    
    So if we can't find a particular file, let's skip it.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- py/travis-ci/travis_tftp.py
+++ py/travis-ci/travis_tftp.py
@@ -4,6 +4,9 @@ import binascii
 def file2env(file_name):
     file_full = os.environ['UBOOT_TRAVIS_BUILD_DIR'] + "/" + file_name
 
+    if not os.path.isfile(file_full):
+        return None
+
     return {
         "fn": file_name,
         "size": os.path.getsize(file_full),
