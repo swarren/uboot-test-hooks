73b2586232e4e8977efff63668a8f170651bb6ec
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2017-09-01 19:26:59 +0300

    travis-ci: Add qemu_arm board
    This is similar to the existing vexpress QEMU targets, except:
    
    - U-Boot is loaded via '-bios u-boot.bin' instead of '-kernel u-boot'
    - Network comes from an e1000 on the PCI bus (like on e.g. qemu-x86_na)
    
    Signed-off-by: Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>

---
+++ py/travis-ci/u_boot_boardenv_qemu_arm_na.py
@@ -0,0 +1,8 @@
+import travis_tftp
+
+env__net_uses_pci = True
+env__net_dhcp_server = True
+
+env__net_tftp_readable_file = travis_tftp.file2env('u-boot.bin')
+env__efi_loader_helloworld_file = travis_tftp.file2env('lib/efi_loader/helloworld.efi')
+env__efi_loader_grub_file = travis_tftp.file2env('grub_arm.efi')
