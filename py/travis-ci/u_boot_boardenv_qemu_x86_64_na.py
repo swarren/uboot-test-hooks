931880234f5fe1f29eccda9cd409862fceed56e2
Bin Meng <bmeng.cn@gmail.com>
2018-10-14 00:20:16 +0800

    travis-ci: Update grub file name for qemu-x86_64 target
    grub_x86.efi is for 32-bit QEMU. Use grub_x64.efi for 64-bit.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- py/travis-ci/u_boot_boardenv_qemu_x86_64_na.py
+++ py/travis-ci/u_boot_boardenv_qemu_x86_64_na.py
@@ -7,4 +7,4 @@ env__net_tftp_readable_file = travis_tftp.file2env('u-boot')
 env__efi_loader_helloworld_file = travis_tftp.file2env('lib/efi_loader/helloworld.efi')
 
 env__efi_loader_check_smbios = True
-env__efi_loader_grub_file = travis_tftp.file2env('grub_x86.efi')
+env__efi_loader_grub_file = travis_tftp.file2env('grub_x64.efi')
