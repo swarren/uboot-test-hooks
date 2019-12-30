969732ba0067b85c70433c9111d9797eed860d88
Cristian Ciocaltea <cristian.ciocaltea@gmail.com>
2019-12-30 13:49:49 +0200

    travis-ci: Fix "ResourceWarning: unclosed file"
    This patch gets rid of the warning messages like:
    
    uboot-test-hooks/py/travis-ci/travis_tftp.py:43: ResourceWarning:
    unclosed file <_io.BufferedReader name='.bm-work/qemu_arm/u-boot.bin'>
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@gmail.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- py/travis-ci/travis_tftp.py
+++ py/travis-ci/travis_tftp.py
@@ -20,8 +20,11 @@ def file2env(file_name, addr=None):
     ret = {
         "fn": file_name,
         "size": os.path.getsize(file_full),
-        "crc32": hex(binascii.crc32(open(file_full, 'rb').read()) & 0xffffffff)[2:],
     }
+
+    with open(file_full, 'rb') as fd:
+        ret["crc32"] = hex(binascii.crc32(fd.read()) & 0xffffffff)[2:]
+
     if addr is not None:
         ret['addr'] = addr
 
