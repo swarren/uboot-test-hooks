6575537d047e3390a4cd2f14dafae2ae78ce8779
Stephen Warren <swarren@nvidia.com>
2020-02-21 13:16:43 -0700

    travis-ci: be consistent about blank lines between values
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- py/travis-ci/u_boot_boardenv_evb-ast2500_qemu.py
+++ py/travis-ci/u_boot_boardenv_evb-ast2500_qemu.py
@@ -1,7 +1,5 @@
 import travis_tftp
 
 env__spl_skipped = True
-
 env__net_dhcp_server = True
-
 env__net_tftp_readable_file = travis_tftp.file2env('u-boot')
--- py/travis-ci/u_boot_boardenv_qemu_x86_na.py
+++ py/travis-ci/u_boot_boardenv_qemu_x86_na.py
@@ -3,13 +3,10 @@ import travis_tftp
 
 env__net_uses_pci = True
 env__net_dhcp_server = True
-
 env__net_tftp_readable_file = travis_tftp.file2env('u-boot')
 env__efi_loader_helloworld_file = travis_tftp.file2env('lib/efi_loader/helloworld.efi')
-
 env__efi_loader_check_smbios = True
 env__efi_loader_grub_file = travis_tftp.file2env('grub_x86.efi')
-
 env__efi_fit_tftp_file = {
     "dn" : os.environ['UBOOT_TRAVIS_BUILD_DIR'],
 }
